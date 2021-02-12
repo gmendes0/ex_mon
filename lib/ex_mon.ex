29921f33bb5be5c6e63804a40e49a63034a2fede
mendesg0 <gabriel@prestige.com.br>
2021-02-12 11:20:37 -0300

    feat: create game agent

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,5 +1,19 @@
 defmodule ExMon do
+  alias ExMon.Game
+  alias ExMon.Game.Status
+  alias ExMon.Player, as: Player
+
+  @computer_name "computer"
+
   def create_player(name, move_average, move_random, move_heal) do
-    ExMon.Player.build name, move_average, move_random, move_heal
+    Player.build name, move_average, move_random, move_heal
+  end
+
+  def start_game(player) do
+    @computer_name
+    |> create_player(:bite, :bijudama, :heal)
+    |> Game.start(player)
+
+    Status.print_round_message()
   end
 end
---
+++ lib/game.ex
@@ -0,0 +1,16 @@
+defmodule ExMon.Game do
+  use Agent
+
+  def start(computer, player) do
+    Agent.start_link fn -> %{
+      computer: computer,
+      player: player,
+      turn: player,
+      status: :started
+    } end, name: __MODULE__
+  end
+
+  def info do
+    Agent.get __MODULE__, & &1
+  end
+end
